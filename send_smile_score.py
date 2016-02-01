869de7a14f456e724727bc83b68931c5864f3ef5
rei___yu <satomi.i.sakura@gmail.com>
2016-02-01 09:14:56 +0900

    modify the way of send post request

--- send_smile_score.py
+++ send_smile_score.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+import time
 
 for line in open('~/Desktop/smile_tmp/score.txt', 'r'):
     scoreList = line[:-1].split('\n')
 
 average = sum(scoreList)/len(scoreList)
 
-req_str = 'http://pom.l-u-l.tk/api' + average #合ってなさそう
-req = urllib2.Request(req_str)
+## yyyy/mm/dd format
+date_in_format = time.strftime("%Y/%m/%d"))
+project_num = 1 # dummy
+req_url = 'http://pom.l-u-l.tk/api/photo_score'
+req.add_header('Content-Type', 'application/json')
+
+data = {
+date: date_in_format
+project_id: project_num
+value: average
+}
+
+response = requests.post(req_url, data=json.dumps(data), headers=headers)
